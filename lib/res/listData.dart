f91268e2741f206999b7a47242f1e2bd3ad4009a
ngooigypngooigyp <1960163740@qq.com>
2019-06-27 20:17:00 +0800

    init

---
+++ lib/res/listData.dart
@@ -0,0 +1,32 @@
+List listData=[
+  {
+    "title": 'Candy Shop',
+    "author": 'Mohamed Chahin',
+    "imageurl": 'images/li.png',
+  },
+  {
+    "title": 'Candy Shop',
+    "author": 'Mohamed Chahin',
+    "imageurl": 'images/li.png',
+  },
+  {
+    "title": 'Candy Shop',
+    "author": 'Mohamed Chahin',
+    "imageurl": 'images/li.png',
+  },
+  {
+    "title": 'Candy Shop',
+    "author": 'Mohamed Chahin',
+    "imageurl": 'images/li.png',
+  },
+  {
+    "title": 'Candy Shop',
+    "author": 'Mohamed Chahin',
+    "imageurl": 'images/li.png',
+  },
+  {
+    "title": 'Candy Shop',
+    "author": 'Mohamed Chahin',
+    "imageurl": 'images/li.png',
+  },
+];

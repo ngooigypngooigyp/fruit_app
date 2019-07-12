ee26d1de24d6c3f8a4e271980cb0c99f99c48e57
ngooigypngooigyp <1960163740@qq.com>
2019-07-12 19:44:42 +0800

    顶部导航

--- lib/pages/tabs/category.dart
+++ lib/pages/tabs/category.dart
@@ -10,26 +10,112 @@ class CategoryPage extends StatefulWidget {
 class _CategoryPageState extends State<CategoryPage> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        RaisedButton(
-          child: Text('跳转到表单页面'),
-          onPressed: (){
-            // Navigator.of(context).push(
-            //   MaterialPageRoute(
-            //      builder: (context){
-            //        return FormPage(title: '我是跳转传值');
-            //      }
-            //   ),
-            // );
-            Navigator.pushNamed(context, '/form');
-          },
-          color: Theme.of(context).accentColor,
-          textTheme: ButtonTextTheme.primary,
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Row(
+            children: <Widget>[
+              Expanded(
+                child: TabBar(
+                  indicatorColor: Colors.red,
+                  labelColor: Colors.red,
+                  unselectedLabelColor: Colors.white,
+                  indicatorSize: TabBarIndicatorSize.label,
+                  indicatorWeight: 1.0,
+                  tabs: <Widget>[
+                    Tab(text: '热销'),
+                    Tab(text: '推荐'),
+                    Tab(text: '精品'),
+                    Tab(text: '国际'),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
-      ],
-    );
+        body: TabBarView(
+          children: <Widget>[
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                RaisedButton(
+                  child: Text('跳转到表单页面'),
+                  onPressed: (){
+                    // Navigator.of(context).push(
+                    //   MaterialPageRoute(
+                    //      builder: (context){
+                    //        return FormPage(title: '我是跳转传值');
+                    //      }
+                    //   ),
+                    // );
+                    Navigator.pushNamed(context, '/form');
+                  },
+                  color: Theme.of(context).accentColor,
+                  textTheme: ButtonTextTheme.primary,
+                ),
+              ],
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                RaisedButton(
+                  child: Text('跳转到表单页面'),
+                  onPressed: (){
+                    Navigator.pushNamed(context, '/form');
+                  },
+                  color: Theme.of(context).accentColor,
+                  textTheme: ButtonTextTheme.primary,
+                ),
+              ],
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                RaisedButton(
+                  child: Text('跳转到表单页面'),
+                  onPressed: (){
+                    // Navigator.of(context).push(
+                    //   MaterialPageRoute(
+                    //      builder: (context){
+                    //        return FormPage(title: '我是跳转传值');
+                    //      }
+                    //   ),
+                    // );
+                    Navigator.pushNamed(context, '/form');
+                  },
+                  color: Theme.of(context).accentColor,
+                  textTheme: ButtonTextTheme.primary,
+                ),
+              ],
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                RaisedButton(
+                  child: Text('跳转到表单页面'),
+                  onPressed: (){
+                    // Navigator.of(context).push(
+                    //   MaterialPageRoute(
+                    //      builder: (context){
+                    //        return FormPage(title: '我是跳转传值');
+                    //      }
+                    //   ),
+                    // );
+                    Navigator.pushNamed(context, '/form');
+                  },
+                  color: Theme.of(context).accentColor,
+                  textTheme: ButtonTextTheme.primary,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );    
   }
 }
